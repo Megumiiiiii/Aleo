e9bb639a85a211a3fdfd2f05e57e8ee32e2d60e0
megumii <98658943+Megumiiiiii@users.noreply.github.com>
2022-11-12 18:09:48 +0700

    Update aleo.sh

--- aleo.sh
+++ aleo.sh
@@ -42,7 +42,6 @@ echo -e "\e[1m\e[32m3. Install Rupstup... \e[0m" && sleep 1
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 echo -e "\e[1m\e[32m4. Clone Repository... \e[0m" && sleep 1
-cd $HOME
 git clone https://github.com/AleoHQ/snarkOS.git --depth 1
 cd ~/snarkOS
 
