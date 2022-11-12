bdf7b137b01347df6b1540abd08cb11f0f60ae33
megumii <98658943+Megumiiiiii@users.noreply.github.com>
2022-11-12 21:33:10 +0700

    Update aleo.sh

--- aleo.sh
+++ aleo.sh
@@ -39,7 +39,7 @@ echo -e "\e[1m\e[32m2. Open Port... \e[0m" && sleep 1
 ufw allow 22 && ufw allow 4133 && ufw allow 3033 && ufw enable
 
 echo -e "\e[1m\e[32m3. Install Rupstup... \e[0m" && sleep 1
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl https://sh.rustup.rs -sSf | sh
 
 echo -e "\e[1m\e[32m4. Clone Repository... \e[0m" && sleep 1
 git clone https://github.com/AleoHQ/snarkOS.git --depth 1
